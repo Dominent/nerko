806ae70e7f98f580f1dbfc5ddb320a70b2188ef2
Dominent <petromilpavlov@gmail.com>
2017-07-16 16:21:23 +0300

    Pre Redux cleanup

--- src/components/pages/board/index.js
+++ src/components/pages/board/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Row from '../../lib/row';
 import Col from '../../lib/col';
@@ -7,75 +7,89 @@ import Sidebar from './sidebar';
 import Section from './section';
 import SectionItem from './section/section-item'
 
-export default () => {
-    return (
-        <div className='board'>
-            <Row>
-                <Col>
-                    <Col md='2'>
-                        <Sidebar />
+export default class Board
+    extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleMouseMoveEvent = this.handleMouseMoveEvent.bind(this);
+    }
+
+    handleMouseMoveEvent(event) {
+        // console.log(event);
+    }
+
+    render() {
+        return (
+            <div className='board'
+                onMouseMove={this.handleMouseMoveEvent}>
+                <Row>
+                    <Col>
+                        <Col md='2'>
+                            <Sidebar />
+                        </Col>
+                        <Col md='9'>
+                            <Col md='3' >
+                                <Section name='ToDo' items={[
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>,
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>,
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>
+                                ]} />
+                            </Col>
+                            <Col md='3' >
+                                <Section name='Doing' items={[
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>,
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>,
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>
+                                ]} />
+                            </Col>
+                            <Col md='3'>
+                                <Section name='Done' items={[
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>,
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>,
+                                    <SectionItem >
+                                        As a user I want to have a personal
+                                account so than nobody but me will
+                                be able to buy from it.
+                            </SectionItem>
+                                ]} />
+                            </Col>
+                        </Col>
                     </Col>
-                    <Col md='9'>
-                        <Col md='3' >
-                            <Section name='ToDo' items={[
-                                <SectionItem >
-                                As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>,
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>,
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>
-                            ]} />
-                        </Col>
-                        <Col md='3' >
-                            <Section name='Doing' items={[
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>,
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>,
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>
-                            ]} />
-                        </Col>
-                        <Col md='3'>
-                            <Section name='Done' items={[
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>,
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>,
-                                <SectionItem >
-                                    As a user I want to have a personal
-                                account so than nobody but me will
-                                be able to buy from it.
-                            </SectionItem>
-                            ]} />
-                        </Col>
-                    </Col>
-                </Col>
-            </Row>
-        </div>
-    )
+                </Row>
+            </div>
+        )
+    }
 }
--- src/components/pages/board/section/section-item/index.js
+++ src/components/pages/board/section/section-item/index.js
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Row from '../../../../lib/row';
 import Col from '../../../../lib/col';
 
 require('./style.scss');
 
-export default ({ children }) => {
-    return (
-        <div className='section-item'>
-            <div className='well'>
-                <Row>
-                    <Col md='4'>
-                        <a href="#" className='thumbnail'>
-                            <img src="https://dummyimage.com/100x100/c4c4c4/fff" alt="avatar"/>
-                        </a>
-                    </Col>
-                    <Col md='8'>
-                        {children}
-                    </Col>
-                </Row>
-            </div>
-        </div>
-    )
+export default class SectionItem
+    extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            position: null
+        }
+
+        this.handleClickEvent = this.handleClickEvent.bind(this);
+    }
+
+    handleClickEvent(event) {
+        var pos = event.target.getBoundingClientRect();
+
+        this.setState({
+            position: {
+                top: pos.top,
+                left: pos.left
+            }
+        })
+
+        console.log(event.target.getBoundingClientRect());
+    }
+
+    render() {
+        return (
+            <div
+                className='section-item'
+                onClick={this.handleClickEvent}
+                style=
+                {
+                    this.state.position ? {
+                        position: "fixed",
+                        top: this.state.position.top,
+                        left: this.state.position.left,
+                        zIndex: 100
+                    } : null
+                }>
+                <div className='well'>
+                    <Row>
+                        <Col lg='4'>
+                            <a href="#" className='thumbnail'>
+                                <img src="https://dummyimage.com/100x100/c4c4c4/fff" alt="avatar" />
+                            </a>
+                        </Col>
+                        <Col lg='8'>
+                            {this.props.children}
+                        </Col>
+                    </Row>
+                </div>
+            </div >
+        )
+    }
 }
